1c9f0b934f98834b25d06b542c105600ae9dbc06
Tulitu Vlad <tulituvlad@gmail.com>
2023-09-20 14:52:10 +0300

    Added CountriesScreen.kt

---
+++ app/src/main/java/com/tully/graphqlcountriesapp/presentation/CountriesScreen.kt
@@ -0,0 +1,66 @@
+package com.tully.graphqlcountriesapp.presentation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tully.graphqlcountriesapp.domain.SimpleCountry
+
+@Composable
+fun CountriesScreen(
+    state:CountriesViewModel.CountriesState,
+    onSelectCountry:(code:String)->Unit,
+    onDismissCountryDialog:()->Unit
+){
+    Box(modifier = Modifier.fillMaxSize()){
+        if(state.isLoading){
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }else{
+            LazyColumn (modifier = Modifier.fillMaxSize()){
+                items(state.countries){
+                    country-> CountryItem(country = country, modifier = Modifier.fillMaxWidth().clickable { onSelectCountry(country.code) }.padding(16.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CountryItem(
+    country:SimpleCountry,
+    modifier: Modifier=Modifier
+){
+    Row(modifier=modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text=country.emoji,
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(modifier = Modifier.weight(1f)){
+            Text(
+                text = country.name,
+                fontSize = 24.sp
+            )
+            Spacer(modifier=Modifier.width(16.dp))
+            Text(text = country.capital)
+        }
+    }
+}
