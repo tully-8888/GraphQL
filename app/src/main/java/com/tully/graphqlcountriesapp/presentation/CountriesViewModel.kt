aea596fd6bfd9c1bb30c9378f962f166f05e667d
Tulitu Vlad <tulituvlad@gmail.com>
2023-09-20 14:40:13 +0300

    Added CountriesViewModel.kt

---
+++ app/src/main/java/com/tully/graphqlcountriesapp/presentation/CountriesViewModel.kt
@@ -0,0 +1,54 @@
+package com.tully.graphqlcountriesapp.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tully.graphqlcountriesapp.domain.DetailedCountry
+import com.tully.graphqlcountriesapp.domain.GetCountriesUseCase
+import com.tully.graphqlcountriesapp.domain.GetCountryUseCase
+import com.tully.graphqlcountriesapp.domain.SimpleCountry
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CountriesViewModel(
+    private val getCountriesUseCase: GetCountriesUseCase,
+    private val getCountryUseCase: GetCountryUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CountriesState())
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+            _state.update {
+                it.copy(
+                    countries = getCountriesUseCase.execute(),
+                    isLoading = false
+                )
+            }
+        }
+    }
+
+    fun selectCountry(code: String) {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    selectedCountry = getCountryUseCase.execute(code)
+                )
+            }
+        }
+    }
+
+    fun dismissCountryDialog() {
+        _state.update { it.copy(selectedCountry = null) }
+    }
+
+    data class CountriesState(
+        val countries: List<SimpleCountry> = emptyList(),
+        val isLoading: Boolean = false,
+        val selectedCountry: DetailedCountry? = null
+    )
+
+}
